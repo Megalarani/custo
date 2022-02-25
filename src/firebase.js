e297687070f3117b3e49161dcb78b44525658e80
campuzone <campuzoneschools@gmail.com>
2022-02-25 20:30:39 +0530

    sundar

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCDSN5B3JCDCYcacfmL29ox4SUxc1HpgKg",
+    authDomain: "custom-9aad1.firebaseapp.com",
+    projectId: "custom-9aad1",
+    storageBucket: "custom-9aad1.appspot.com",
+    messagingSenderId: "288280541056",
+    appId: "1:288280541056:web:cfb54a4b509fe818caf365",
+    measurementId: "G-1YQQM6JN22"
+  };
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+const auth = getAuth(firebase);
+const db = getFirestore(firebase);
+const storage = getStorage(firebase);
+
+export default firebase;
+export { auth, db, analytics, storage };
