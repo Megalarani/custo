b19d6776de250dd7cde851b3a61e952416e6d3a8
Akhil <61401774+Ak-SK@users.noreply.github.com>
2022-03-04 13:07:09 +0530

    milestone completed

--- src/Components/Sections/Footer/Footer1.js
+++ src/Components/Sections/Footer/Footer1.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const Footer1 = () => {
+  const data = {
+    container: {
+      style: "ft-col",
+      value: "",
+    },
+    heading: {
+      style: "",
+      value: "Copyrights©Divicare",
+    },
+  };
   return (
     <>
       <footer>
-        <div class="ft-col">
-          <h2>Copyrights©Divicare </h2>
+        <div class={data.container.style}>
+          <h2 className={data.heading.style}>{data.heading.value}</h2>
         </div>
       </footer>
     </>
--- src/Components/Sections/Gallery/Gallery1.js
+++ src/Components/Sections/Gallery/Gallery1.js
@@ -1,9 +1,59 @@
 import React from "react";
 
 const Gallery1 = () => {
-    return (
-        <></>
-    )
-}
+  const data = {
+    container: {
+      styles: "gallery-heading",
+      value: "",
+    },
+    heading: {
+      styles: "",
+      value: "Gallery",
+    },
+    images: [
+      {
+        imgName: "Sea",
+        imgLink:
+          "https://images.unsplash.com/photo-1600345957894-4854e82910ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80",
+      },
+      {
+        imgName: "Sea",
+        imgLink:
+          "https://images.unsplash.com/photo-1600345957894-4854e82910ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80",
+      },
+      {
+        imgName: "Sea",
+        imgLink:
+          "https://images.unsplash.com/photo-1600345957894-4854e82910ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80",
+      },
+      {
+        imgName: "Sea",
+        imgLink:
+          "https://images.unsplash.com/photo-1600345957894-4854e82910ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1748&q=80",
+      },
+    ],
+  };
+  return (
+    <>
+      <section id="#Gallery">
+        <div class="gallery-heading">
+          <h2>Gallery</h2>
+        </div>
+
+        <div class="gallery">
+          {data.images.map((item, index) => (
+            <a href="#" class="gal" key={index}>
+              <div class="gal-wrapper">
+                <div class="hidden-cover"></div>
+                <img src={item.imgLink} alt={item.imgName} />
+                <h4>{item.imgName}</h4>
+              </div>
+            </a>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Gallery1;
