0a18c608bbb8ef617b508d289a79ca6e8bed75e7
Akhil <61401774+Ak-SK@users.noreply.github.com>
2022-03-08 14:27:37 +0530

    message

--- src/loader/Loader.js
+++ src/loader/Loader.js
@@ -1,15 +1,17 @@
-import "./Loader.css"
-const Loader = () =>{
-    return(<>
-    <div className="loader-con">
-    <div class="loader-wrapper">
-  <div class="loader">
-    <span class="circle"></span>
-    <span class="triangle"></span>
-    <span class="sqaure"></span>
-  </div>
-</div>
-</div>
-    </>)
-}
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <>
+      <div className="loader-con">
+        <div class="loader-wrapper">
+          <div class="loader">
+            <span class="circle"></span>
+            <span class="triangle"></span>
+            <span class="sqaure"></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Loader;
