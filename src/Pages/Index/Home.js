d44e7bc3916c3697ea9e08260716de2e22b76819
campuzone <campuzoneschools@gmail.com>
2022-03-12 14:50:50 +0530

    sundar

--- src/Pages/Index/Home.js
+++ src/Pages/Index/Home.js
@@ -2,26 +2,26 @@ import React from "react";
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../../Context/Context";
 const Home = () => {
-  // const ctx = useContext(AuthContext);
-  // const [mountedData, setMountedData] = useState([]);
-  // useEffect(() => {
-  //   ctx.getLayoutData();
-  //   ctx.getWebsiteData();
-  //   setMountedData(ctx.layoutFlow ? ctx.layoutFlow : []);
-  // }, [mountedData]);
+  const ctx = useContext(AuthContext);
+  const [mountedData, setMountedData] = useState([]);
+  useEffect(() => {
+    ctx.getLayoutData();
+    ctx.getWebsiteData();
+    setMountedData(ctx.layoutFlow ? ctx.layoutFlow : []);
+  }, [mountedData]);
 
-  // const CreateComponent = ({ component }) => {
-  //   const Component = component;
-  //   return <Component />;
-  // };
+  const CreateComponent = ({ component }) => {
+    const Component = component;
+    return <Component />;
+  };
 
   return (
     <>
-      {/* {mountedData.map((item, index) => (
+      {mountedData.map((item, index) => (
         <div>
           <CreateComponent component={item.c} />
         </div>
-      ))} */}
+      ))}
     </>
   );
 };
